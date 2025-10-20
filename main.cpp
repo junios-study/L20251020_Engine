cb0dcb54e8bf1d87525d3c7f38c8f88aa54e36bd
junios-study <junios@naver.com>
2025-10-20 11:44:04 +0900

    엔진 만들기
    맵파일 생성

--- main.cpp
+++ main.cpp
@@ -1,32 +1,16 @@
-#include <iostream>
-#include "Actor.h"
-#include "Player.h"
-
-#include <vector> //STL
-
-using namespace std;
+#include "Engine.h"
 
 
-
-int main()
+int main(int argc, char* argv[])
 {
-	//자료 구조. 메모리에 저장하기 위한 방법, Strandart Template Library(STL) C++, boost::asio(IOCP, epoll)
-	vector<AActor*> Actors;
-	Actors.push_back(new AActor());
-	Actors.push_back(new APlayer());
+	FEngine* MyEngine = new FEngine();
 
-	//for (int i = 0; i < Actors.size(); ++i)
-	for (auto Data : Actors)
-	{
-		Data->Tick(); // 형태 바뀜, 형태 많다. 다형성, Polymophism
-	}
-	 
-	for (int i = 0; i < Actors.size(); ++i)
-	{
-		delete Actors[i];
-	}
-	Actors.clear();
+	MyEngine->Init();
+	MyEngine->Run();
+	MyEngine->Term();
 
+	delete MyEngine;
+	MyEngine = nullptr;
 
 	return 0;
 }
